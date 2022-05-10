4a59e00fba8d5a123df3c96bdb4c504ab16767cb
jiangzhangxiang <yyy67239667@163.com>
2022-05-10 11:17:24 +0800

    feat: 岗位列表 ts 类型修复

--- src/pages/system/Post/data.d.ts
+++ src/pages/system/Post/data.d.ts
@@ -1,15 +1,8 @@
 export type PostListItem = {
   postId?: number;
-  nickName?: string;
-  password?: string;
-  postIds?: any[];
-  roleIds?: any[];
   status?: '0' | '1' | '2';
   userName?: string;
   createTime?: string;
-  dept?: {
-    deptName?: string;
-  };
 };
 
 export type PostList = {
