d20fa168e47a9477507ba67884d233177673f9f8
jiangzhangxiang <yyy67239667@163.com>
2022-05-18 23:35:23 +0800

    feat: 修改环境配置

---
+++ config/config.prod.ts
@@ -0,0 +1,8 @@
+// https://umijs.org/config/
+import { defineConfig } from 'umi';
+const { REACT_APP_ENV } = process.env;
+export default defineConfig({
+  define: {
+    REACT_APP_ENV: REACT_APP_ENV || 'dev',
+  },
+});
