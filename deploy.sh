53b72e7a80062e5408716380864ec533bd3e9a2a
jiangzhangxiang <yyy67239667@163.com>
2022-06-27 10:40:53 +0800

    feat(deploy): 脚本自动升级

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env sh
-
 # 确保脚本抛出遇到的错误
 set -e
-
 # 提交代码测试
 git add -A
-git commit -m "fix(deploy): 脚本自动升级"
+git commit -m "feat(deploy): 脚本自动升级"
 git pull
 git push origin master
 
 ## 发布到阿里云
-#npm build:prod
-#ssh root@jzx -C "rm -rf /home/web/ruoyi/dist"
-#scp -r dist root@jzx:/home/web/ruoyi/dist
-#
-#exit
+npm build:prod
+ssh root@121.43.63.77 -C "rm -rf /home/web/ruoyi/dist"
+scp -r dist root@121.43.63.77:/home/web/ruoyi/dist
+echo "发布成功"
+exit
