a2a6fc0c7bc3425e91f46da70ea4d1597d1c0cc4
jiangzhangxiang <yyy67239667@163.com>
2022-06-27 22:49:47 +0800

    feat(deploy): 脚本自动升级

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,58 @@
-#!/usr/bin/env sh
-# 确保脚本抛出遇到的错误
-set -e
-# 提交代码测试
-git add -A
-git commit -m "feat(deploy): 脚本自动升级"
-git pull
-git push origin master
+#!/bin/bash
 
-## 发布到阿里云
-yarn build:prod
-ssh root@121.43.63.77 -C "rm -rf /home/web/ruoyi/dist"
-scp -r dist root@121.43.63.77:/home/web/ruoyi/dist
-echo "发布成功"
-exit
+echo " 脚本开始😎"
+
+if branch=$(git symbolic-ref --short -q HEAD)
+
+then
+  echo '当前分支['$branch']'
+else
+  echo '不在任何分支上,脚本结束...'
+  exit 1
+fi
+
+git add .
+echo "请输入提交注释? [Enter] 'feat(deploy): 脚本自动升级'"
+
+read commit_name
+  if [ -z "$commit_name" ]
+  then
+  commit_name="feat(deploy): 脚本自动升级"
+  fi
+echo "git提交注释:$commit_name"
+
+git commit -m $commit_name
+git pull
+git push
+
+read -r -p "是否升级到测试服务器? [环境参数][Enter(prod)][n(取消)] " input
+
+case $input in
+    [nN][oO]|[nN])
+		echo " 取消升级脚本❌ ️"
+		exit 1
+       		;;
+    *)
+	;;
+esac
+#
+#if [ -z "$input" ]
+#then
+#input="prod"
+#fi
+#
+#echo 'branch['$branch']env['$input"]正在升级..."
+#
+#npm run build:$input
+#ssh root@yx -C "rm -rf /home/web/ruoyi/dist"
+#scp -r dist root@yx:/home/web/ruoyi/dist
+#
+#echo " 升级完成✅ "
+
+
+#ssh yx -tt << EOF
+#cd /home/git/architecture-api
+#git pull
+#npm run build:$input
+#exit
+#EOF
