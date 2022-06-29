cebadcb4fafe81301ebecde7da1bc72b356d0822
jiangzhangxiang <yyy67239667@163.com>
2022-06-29 10:12:53 +0800

    feat(deploy): 脚本自动升级

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -89,7 +89,7 @@ export async function blobValidate(data: any) {
  * 转换字符串，undefined,null等转化为""
  * @param str
  */
-export function parseStrEmpty(str: string) {
+export function parseStrEmpty(str?: string | number) {
   if (!str || str == 'undefined' || str == 'null') {
     return '';
   }
