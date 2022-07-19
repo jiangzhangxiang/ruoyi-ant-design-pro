d4d40d57adffc3e90c97e1092355d226332a52d1
jiangzhangxiang <yyy67239667@163.com>
2022-07-19 20:45:25 +0800

    feat(deploy): 脚本自动升级

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -36,7 +36,6 @@ const Admin: React.FC = () => {
         <a href="https://pro.ant.design/docs/block-cn" target="_blank" rel="noopener noreferrer">
           use block
         </a>
-        。
       </p>
     </PageHeaderWrapper>
   );
