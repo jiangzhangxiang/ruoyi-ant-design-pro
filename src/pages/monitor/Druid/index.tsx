3af50a20c0828694cc0321925469c0bf55b3f1e3
jiangzhangxiang <yyy67239667@163.com>
2022-12-01 15:25:52 +0800

    feat: druid 页面

--- src/pages/monitor/Druid/index.tsx
+++ src/pages/monitor/Druid/index.tsx
@@ -3,11 +3,7 @@ import React from 'react';
 import Iframe from '@/components/Iframe';
 import { APP_BASE_API } from '@/utils/evn';
 
-type IframeProps = {
-  src: string;
-};
-
-const TableList: React.FC<IframeProps> = () => {
+const TableList: React.FC = () => {
   const prefix = APP_BASE_API[REACT_APP_ENV] || '';
   const src = `${prefix}/druid/login.html`;
   return (
